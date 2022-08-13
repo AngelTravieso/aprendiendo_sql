e4c5e49a86f6f5889b1ed7dd1c1a6e3c0e2a72e9
AngelTravieso <angeltraviesoc@gmail.com>
2022-08-13 18:01:37 -0400

    operaciones

--- 04-dml/select.sql
+++ 04-dml/select.sql
@@ -3,3 +3,11 @@
 
 -- MOSTRAR TODOS LOS REGISTROS / FILAS DE UNA TABLA
 SELECT email, nombre, apellidos FROM usuarios;
+
+-- OPERADORES ARITMÉTICOS
+SELECT email, (4 + 7) AS operacion FROM usuarios;
+
+-- ORDENAR DATOS
+SELECT id, email, (4 + 7) AS operacion FROM usuarios ORDER BY id DESC;
+
+

e980a299c393c1faf7937c3832fe5d1041e379c5
AngelTravieso <angeltraviesoc@gmail.com>
2022-08-16 21:18:15 -0400

    funciones para fechas

--- 04-dml/select.sql
+++ 04-dml/select.sql
@@ -60,3 +60,45 @@ SELECT email, LENGTH(CONCAT(nombre, ' ', apellidos)) AS conversion FROM usuarios
 
 -- QUITAR ESPACIOS EN BLANCO
 SELECT TRIM(CONCAT(nombre, '           ', apellidos, '                            ')) AS conversion FROM usuarios;
+
+
+-- FUNCIONES PARA FECHAS
+
+-- FECHA ACTUAL (CURRENT DATE)
+SELECT email, fecha_registro, CURDATE() AS 'fecha_actual' FROM usuarios;
+
+-- VER LA DIFERENCIA ENTRE 2 FECHAS
+SELECT email, DATEDIFF(fecha_registro, CURDATE()) AS 'diferencia_fecha' FROM usuarios;
+
+-- NOMBRE DEL DIA EN EL CUAL SE REGISTRO (INSERTO) EN INGLES
+SELECT email, DAYNAME(fecha_registro) AS 'dia' FROM usuarios;
+
+-- NOMBRE DEL MES EN EL CUAL SE REGISTRO (INSERTO) EN INGLES
+SELECT email, DAYOFMONTH(fecha_registro) AS 'mes' FROM usuarios;
+
+-- DÍA DE LA SEMANA EN EL CUAL SE REGISTRO (INSERTO) EN INGLES
+SELECT email, DAYOFWEEK(fecha_registro) AS 'semana' FROM usuarios;
+
+-- DÍA DEL AÑO EN EL CUAL SE REGISTRO (INSERTO) EN INGLES
+SELECT email, DAYOFYEAR(fecha_registro) AS 'numero_dia' FROM usuarios;
+
+-- MES DEL AÑO
+SELECT email, MONTH(fecha_registro) AS 'month_of_year' FROM usuarios;
+
+-- AÑO DE ESA FECHA
+SELECT email, YEAR(fecha_registro) AS 'month_of_year' FROM usuarios;
+
+-- DÍA DE ESA FECHA
+SELECT email, DAY(fecha_registro) AS 'month_of_year' FROM usuarios;
+
+-- HORA DE ESA FECHA
+SELECT email, HOUR(fecha_registro) AS 'month_of_year' FROM usuarios;
+
+-- HORA ACTUAL
+SELECT email, CURTIME() AS 'hour' FROM usuarios;
+
+-- HORA SO / SERVIDOR
+SELECT email, SYSDATE() AS 'hour_so' FROM usuarios;
+
+-- FORMATEAR FECHA
+SELECT email, DATE_FORMAT(fecha_registro, '%d-%m-%y') AS 'hour_so' FROM usuarios;
